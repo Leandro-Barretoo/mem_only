f577e293b01955008aff9dc58c50516895c2c181
oluwadare juwon <wintan1418@gmail.com>
2021-02-16 15:48:18 +0100

    add routing for user and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
   resources :posts, only: [:new, :create, : index]
-  resources :user, only: [:new, :create, :index]
+  resources :user
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
