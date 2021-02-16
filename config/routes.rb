0041f5f27b90210e7365683d8ccc8129e9ddfdf3
oluwadare juwon <wintan1418@gmail.com>
2021-02-16 15:57:39 +0100

    add before_action to the post controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts, only: [:new, :create, : index]
   resources :user
+  resources :posts, only: [:new, :create, : index]
+end
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
