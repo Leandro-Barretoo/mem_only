dd9605b3bd3f978eb00030090476b24df10c0ffa
Leandro-Barretoo <officialdevmi@gmail.com>
2021-02-18 07:24:46 +0000

    Finished the project functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.id = current_user.id
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to root_path
     else
